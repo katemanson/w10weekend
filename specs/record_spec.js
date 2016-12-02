41f59725c0d2c9dc26fce9356d326aad6f913d88
katemanson <katemanson107@hotmail.com>
2016-12-02 10:58:50 +0000

    initial commit

---
+++ specs/record_spec.js
@@ -0,0 +1,23 @@
+var Record = require("../record");
+var assert = require("assert");
+
+describe("Record", function() {
+
+  var recordOne = new Record({
+    artist: "Miles Davis", 
+    title: "Blue Moods", 
+    price: 19.99});
+
+  it("should have an artist name", function() {
+    assert.equal(recordOne.artist, "Miles Davis");
+  });
+
+  it("should have a title", function() {
+    assert.equal(recordOne.title, "Blue Moods");
+  });
+
+  it("should have a price", function() {
+    assert.equal(recordOne.price, 19.99);
+  });
+
+});
