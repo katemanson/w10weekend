68ae3a2587ae56a3b67e3dc9ac2bcb56da563297
katemanson <katemanson107@hotmail.com>
2016-12-02 11:10:50 +0000

    adds record object constructor

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -6,7 +6,26 @@ describe("Record", function() {
   var recordOne = new Record({
     artist: "Miles Davis", 
     title: "Blue Moods", 
-    price: 19.99});
+    price: 19.99
+  });
+
+  var recordTwo = new Record({
+    artist: "Dave Brubeck",
+    title: "Take Five",
+    price: 32.90
+  });
+
+  var recordThree = new Record({
+    artist: "Sonny Rollins",
+    title: "Now's the Time",
+    price: 30.00
+  });
+
+  var recordFour = new Record({
+    artist: "Nina Simone",
+    title: "Silk & Soul",
+    price: 28.17
+  });
 
   it("should have an artist name", function() {
     assert.equal(recordOne.artist, "Miles Davis");
