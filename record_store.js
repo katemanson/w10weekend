10c0d4b4fa4fd5689d25f8a6b7047d368870bf86
katemanson <katemanson107@hotmail.com>
2016-12-03 17:01:39 +0000

    adds listInventory methods (printout-style and JSON), and sale method

--- record_store.js
+++ record_store.js
@@ -6,16 +6,77 @@ var RecordStore = function(params) {
 };
 
 RecordStore.prototype = {
-  
-  addRecord: function(record) {
-    this.inventory.push(record);
+
+  addRecord: function(record, quantity) {
+
+    var index = this.inventory.findIndex(function(item) {
+      return (item[0].artist === record.artist && item[0].title === record.title);
+    });
+
+    if ( index >= 0 ) {
+      this.inventory[index][1].quantity += quantity;
+    }
+    else if ( quantity === undefined ) {
+      this.inventory.push([record, {quantity: 1}]);
+    }
+    else {
+      this.inventory.push([record, {quantity: quantity}]);
+    };
   },
 
-  addRecordMultiple: function(record, number) {
-    for ( var i = 0; i < number; i++ ) {
-      this.inventory.push(record);
+  addRecordsFromList: function(recordsList) {
+    this.inventory = this.inventory.concat(recordsList);
+  }, 
+
+  listInventory: function() {
+
+    var listIntro = "--------------------------------------\n " + this.name + ", " + this.city + ": Inventory\n--------------------------------------";
+    var listingText = "";
+
+    var listings = this.inventory.map( function(listing) {
+      return listingText = "\n\nArtist: " + listing[0].artist + "\nTitle: " + listing[0].title + "\nPrice: £" + listing[0].price + "\nQuantity: " + listing[1].quantity;
+    });
+
+    var finalList = listings.reduce( function(list, listing) {
+      return list.concat(listing);
+    }, listIntro);    
+
+    return finalList;
+  }, 
+
+  listInventoryJSON: function() {
+    return JSON.stringify(this.inventory);
+  },
+
+  sale: function(record, quantity) {
+
+    // .findIndex() returns -1 if none found
+    var index = this.inventory.findIndex(function(item) {
+      return ( item[0].artist === record.artist && item[0].title === record.title && item[1].quantity > 0 );
+    });
+
+    if ( index >= 0 && quantity <= this.inventory[index][1].quantity ) {
+      this.inventory[index][1].quantity -= quantity;
+      this.bankBalance += this.inventory[index][0].price * quantity;
     }
+    else if ( index >= 0 && quantity === undefined && this.inventory[index][1].quantity >= 1 ) {
+      this.inventory[index][1].quantity--;
+      this.bankBalance += this.inventory[index][0].price;
+    }
+    else {
+      if ( index === -1 ) {
+        return "Out of stock";
+      }
+      if ( this.inventory[index][1].quantity < quantity ) {
+        return "Only " + this.inventory[index][1].quantity + " in stock";
+      }
+    };
+
+    if ( this.inventory[index][1].quantity <= 0 ) {
+      this.inventory.splice(index, 1);
+    };
   }
+
 };
 
 module.exports = RecordStore;
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -33,9 +33,14 @@ describe ("RecordStore", function() {
     price: 28.17
   });
 
+  var recordsList = [[recordOne, {quantity: 3}], [recordTwo, {quantity: 2}], [recordThree, {quantity: 1}], [recordFour, {quantity: 1}]];
+
 
   beforeEach(function() {
     storeOne.inventory = [];
+    storeOne.bankBalance = 5000;
+    // ?recordsList below used in tests for sale method; quantity changes between tests unless reset by including here, in beforeEach; not sure why??
+    recordsList = [[recordOne, {quantity: 3}], [recordTwo, {quantity: 2}], [recordThree, {quantity: 1}], [recordFour, {quantity: 1}]];
   });
 
 
@@ -55,35 +60,79 @@ describe ("RecordStore", function() {
     assert.equal(5000, storeOne.bankBalance);
   }); 
 
-  it("should be possible to add a record to inventory", function() {
-    storeOne.addRecord(recordOne);
-    assert.deepEqual(recordOne, storeOne.inventory[0]);
+  it("should be possible to add a record to inventory; no pre-existing entry for the same record", function() {
+    storeOne.addRecord(recordOne, 3);
+    assert.deepEqual(recordOne, storeOne.inventory[0][0]);
+    assert.equal(3, storeOne.inventory[0][1].quantity);
   }); 
 
-  it("should be possible to add multiples of a record to inventory", function() {
-    storeOne.addRecordMultiple(recordOne, 5);
-    assert.equal(5, storeOne.inventory.length);
-    assert.deepEqual(recordOne, storeOne.inventory[4]);
-  }); 
+  it("should be possible to add a record to inventory; entry for the same record already exists", function() {
+    storeOne.addRecord(recordOne, 3);
+    storeOne.addRecord(recordOne, 2);
+    assert.equal(5, storeOne.inventory[0][1].quantity);
+  });
+
+  it("should be possible to add a record to inventory; assume quantity = 1 when no quantity argument given", function() {
+    storeOne.addRecord(recordOne);
+    assert.equal(1, storeOne.inventory[0][1].quantity);
+  });
 
   it("should be possible to add multiple records to inventory", function() {
+    storeOne.addRecordsFromList(recordsList);
+    assert.equal(4, storeOne.inventory.length);
+    assert.deepEqual(recordOne, storeOne.inventory[0][0]);
+    assert.deepEqual(recordFour, storeOne.inventory[3][0]);
+  }); 
+
+  it("should be possible to list the inventory; printout style", function() {
+    storeOne.addRecordsFromList(recordsList);
+    var expected = "--------------------------------------\n Vinyl Villains, Edinburgh: Inventory\n--------------------------------------\n\nArtist: Miles Davis\nTitle: Blue Moods\nPrice: £19.99\nQuantity: 3\n\nArtist: Dave Brubeck\nTitle: Take Five\nPrice: £32.9\nQuantity: 2\n\nArtist: Sonny Rollins\nTitle: Now's the Time\nPrice: £30\nQuantity: 1\n\nArtist: Nina Simone\nTitle: Silk & Soul\nPrice: £28.17\nQuantity: 1";
+    assert.equal(storeOne.listInventory(), expected);
+  }); 
+
+  it("should be possible to list the inventory; JSON", function() {
+    storeOne.addRecordsFromList(recordsList);
+    var expected = '[[{"artist":"Miles Davis","title":"Blue Moods","price":19.99},{"quantity":3}],[{"artist":"Dave Brubeck","title":"Take Five","price":32.9},{"quantity":2}],[{"artist":"Sonny Rollins","title":"Now\'s the Time","price":30},{"quantity":1}],[{"artist":"Nina Simone","title":"Silk & Soul","price":28.17},{"quantity":1}]]';
+    assert.equal(storeOne.listInventoryJSON(), expected);
+  }); 
+
+  it("should change stock levels on selling a record", function() {
+    storeOne.addRecordsFromList(recordsList);
+    storeOne.sale(recordOne, 2);
+    assert.equal(storeOne.inventory[0][1].quantity, 1);
+  }); 
+
+  it("should change stock levels on selling a record; assume quantity = 1 when no quantity argument given", function() {
+    storeOne.addRecordsFromList(recordsList);
+    storeOne.sale(recordOne);
+    assert.equal(storeOne.inventory[0][1].quantity, 2);
+  }); 
+
+  it("should delete record when all copies are sold", function() {
+    storeOne.addRecordsFromList(recordsList);
+    storeOne.sale(recordOne, 3);
+    assert.equal(storeOne.inventory.length, 3);
+    assert.equal(storeOne.inventory[0][0].artist, "Dave Brubeck");
+  });
+
+  it("should return an appropriate message if there isn't enough stock; too few copies", function() {
+    storeOne.addRecord(recordOne, 2);
+    assert.equal(storeOne.sale(recordOne, 3), "Only 2 in stock");
+  });
+
+  it("should return an appropriate message if there isn't enough stock; out of stock", function() {
+    storeOne.addRecord(recordOne, 2);
+    assert.equal(storeOne.sale(recordTwo, 3), "Out of stock");
+  });
+
+  it("should increase bank balance on selling a record", function() {
     assert.equal();
   }); 
 
-  it("should be possible to list inventory", function() {
+  it("should be possible to report on store finances", function() {
     assert.equal();
   }); 
 
-  // it("should be possible to sell a record", function() {
-  //   assert.equal();
-  // }); 
 
-  // it("should increase bank balance on selling a record", function() {
-  //   assert.equal();
-  // }); 
-
-  // it("should be possible to report on store finances", function() {
-  //   assert.equal();
-  // }); 
 
 });
