eda99ee258f61ebb8153fc58596bf2a0624e1c5a
katemanson <katemanson107@hotmail.com>
2016-12-03 18:20:59 +0000

    adds record_collector.js and related spec

---
+++ specs/record_collector_spec.js
@@ -0,0 +1,26 @@
+var RecordCollector = require("../record_collector");
+var assert = require("assert");
+
+describe("RecordCollector", function() {
+
+  it("should have a name", function() {
+    assert.equal();
+  });
+
+  it("should have an address", function() {
+    assert.equal();
+  });
+
+  it("should have a bank balance", function() {
+    assert.equal();
+  });
+
+  it("should have a record collection that starts empty", function() {
+    assert.equal();
+  });
+
+  it("should have a name", function() {
+    assert.equal();
+  });
+
+});
